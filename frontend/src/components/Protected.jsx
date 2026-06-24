94d13d85440ec75cb4d08edc58b1aded282ef9d0
Lakshmi Sivani Potukuchi <lakshmisivani09@gmail.com>
2026-06-24 05:43:19 +0530

    added screenshots

--- frontend/src/components/Protected.jsx
+++ frontend/src/components/Protected.jsx
@@ -1,19 +1,3 @@
-// import { useEffect } from "react";
-// import { useNavigate } from "react-router-dom";
-
-// export default function Protected({ children }) {
-//     const navigate = useNavigate();
-//     useEffect(() => {
-//         const token = localStorage.getItem("token");
-//         if (!token) {
-//             navigate("/login");
-//         }
-//     }, [navigate]);
-//     return <>
-//         {children}
-//     </>
-// }
-
 import { useNavigate } from 'react-router-dom'
 import { useEffect } from 'react'
 
